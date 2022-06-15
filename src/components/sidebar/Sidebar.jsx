5a7aa72142f26c4d2115bbbabb09bbefc9c4a017
Satoshi Matsushima <>
2022-06-15 21:16:04 +0900

    46. Sidebarコンポーネントの雛形をHTMLで作成しよう

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,58 @@
+import {
+  Bookmark,
+  Home,
+  MessageRounded,
+  Notifications,
+  Person,
+  Search,
+  Settings,
+} from '@mui/icons-material'
 import React from 'react'
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Home className="sidebarIcon" />
+            <span className="sidarListItemText">Home</span>
+          </li>
+          <li className="sidebarListItem">
+            <Search className="sidebarIcon" />
+            <span className="sidarListItemText">検索</span>
+          </li>
+          <li className="sidebarListItem">
+            <Notifications className="sidebarIcon" />
+            <span className="sidarListItemText">通知</span>
+          </li>
+          <li className="sidebarListItem">
+            <MessageRounded className="sidebarIcon" />
+            <span className="sidarListItemText">メッセージ</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidarListItemText">ブックマーク</span>
+          </li>
+          <li className="sidebarListItem">
+            <Person className="sidebarIcon" />
+            <span className="sidarListItemText">プロフィール</span>
+          </li>
+          <li className="sidebarListItem">
+            <Settings className="sidebarIcon" />
+            <span className="sidarListItemText">設定</span>
+          </li>
+        </ul>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriendListItem">
+            <img src="/assets/person/2.jpeg" alt="" />
+            <span className="sidebarFriendName">Shin Code</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
