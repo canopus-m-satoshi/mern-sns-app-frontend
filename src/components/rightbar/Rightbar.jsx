551a631dd4355aeea1993fa46a549d9766beebd6
Satoshi Matsushima <>
2022-06-15 23:57:11 +0900

    61. Rightbarのオンラインの友達もダミーデータで更新しよう

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import './Rightbar.css'
 
+import { Users } from '../../dummydata'
+import Online from '../online/Online'
+
 const Rightbar = () => {
   return (
     <div className="rightbar">
@@ -15,28 +18,9 @@ const Rightbar = () => {
         <img src="./assets/event.jpeg" alt="" className="eventImg" />
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                src="./assets/person/1.jpeg"
-                alt=""
-                className="rightbarProfileImg"
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">Shin Code</span>
-          </li>
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img
-                src="./assets/person/2.jpeg"
-                alt=""
-                className="rightbarProfileImg"
-              />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">Tanaka</span>
-          </li>
+          {Users.map((user) => (
+            <Online user={user} key={user.id} />
+          ))}
         </ul>
         <p className="promotionTitle">プロモーション広告</p>
         <img
