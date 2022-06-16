cfebf6ed0c4a29fa8dec6817457ff5e51031c528
Satoshi Matsushima <>
2022-06-16 22:36:46 +0900

    69. プロフィールのRightbarの雛形をHTMLで作成しよう

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -46,7 +46,36 @@ const Rightbar = ({ profile }) => {
   }
 
   const ProfileRightbar = () => {
-    return <>profileのrightbarです</>
+    return (
+      <>
+        <h4 className="rightbarTitle">ユーザー情報</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">出身：</span>
+            <span className="rightbarInfoKey">福岡</span>
+          </div>
+          <h4 className="rightbarTitle">あなたの友達</h4>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img
+                src="./assets/person/1.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Shin Code</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="./assets/person/2.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Yamaki</span>
+            </div>
+          </div>
+        </div>
+      </>
+    )
   }
 
   return (
