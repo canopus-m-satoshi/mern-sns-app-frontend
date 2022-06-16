d2367ff9dff6d9eb5d374052a8e314327f855a2f
Satoshi Matsushima <>
2022-06-16 22:55:15 +0900

    73. ログインのコンポーネントを作成してみよう(その２)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Profile from './pages/profile/Profile'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <div>
-      <Profile />
+      <Login />
     </div>
   )
 }
