05720e4575135a800417fc64f2cc86c54be34856
Satoshi Matsushima <>
2022-06-18 14:38:44 +0900

    86. ダミーユーザーデータをAPIを叩いてデータベースから取得しよう

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,14 +1,24 @@
 import { MoreVert } from '@mui/icons-material'
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 import './Post.css'
-import { Users } from '../../dummydata'
 
 const Post = ({ post }) => {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER
 
   const [like, setLike] = useState(post.like)
   const [isLliked, setIsLike] = useState(false)
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`/users/${post.userId}`)
+      setUser(response.data)
+    }
+
+    fetchUser()
+  }, [])
 
   const handleLike = () => {
     // if (!isLliked) {
@@ -28,17 +38,8 @@ const Post = ({ post }) => {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              src={
-                PUBLIC_FOLDER +
-                Users.filter((user) => user.id === post.id)[0].profilePicture
-              }
-              alt=""
-              className="postProfileImg"
-            />
-            <span className="postUsername">
-              {Users.filter((user) => user.id === post.id)[0].username}
-            </span>
+            <img src={user.profilePicture} alt="" className="postProfileImg" />
+            <span className="postUsername">{user.username}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
