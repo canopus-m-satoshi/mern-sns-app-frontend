44d2c19b895dd0b462ea9b35671a52cf330f6a57
Satoshi Matsushima <>
2022-06-18 12:53:18 +0900

    79. react-router-domでルーティング設定をはじめよう

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import Profile from './pages/profile/Profile'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
       <Home />
-    </div>
+    </Router>
   )
 }
 
