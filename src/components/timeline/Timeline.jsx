66aab5c7c6cfe861831b3e60c93789ae1d91e97f
Satoshi Matsushima <>
2022-06-18 14:17:33 +0900

    85. 実際に自作したAPIをAxiosで叩いてデータを取得してみよう

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 import Post from '../post/Post'
 import Share from '../share/Share'
-
-import { Posts } from '../../dummydata'
 
 import './Timeline.css'
 
 const Timeline = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await axios.get(
+        '/posts/timeline/62a568b1ffb1c8689df84181',
+      )
+      setPosts(response.data)
+    }
+
+    fetchPost()
+  }, [])
+
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {Posts.map((post) => (
+        {posts.map((post) => (
           <Post post={post} key={post.id} />
         ))}
       </div>
