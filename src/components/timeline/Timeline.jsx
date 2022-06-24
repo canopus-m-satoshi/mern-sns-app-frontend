40a225bb4358511bb72da3dd279a67f74a975f03
Satoshi Matsushima <>
2022-06-24 17:58:56 +0900

    s

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import axios from 'axios'
 import Post from '../post/Post'
 import Share from '../share/Share'
-import axios from 'axios'
 
 import './Timeline.css'
 
-const Timeline = ({ username }) => {
+const Timeline = () => {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
     const fetchPost = async () => {
-      const response = username
-        ? await axios.get(`/posts/profile/${username}`)
-        : await axios.get('/posts/timeline/62ad6fd7c473c8640b3d8994')
+      const response = await axios.get(
+        '/posts/timeline/62ad6fd7c473c8640b3d8994',
+      )
       setPosts(response.data)
+      console.log(response.data)
     }
 
     fetchPost()
